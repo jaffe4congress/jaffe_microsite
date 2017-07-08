d86eb93bfdd264038c7b712575eb9eee925f9e8e
Chris Sita <cs.sita@me.com>
2017-07-08 06:05:22 -0700

    Added static hapi server

---
+++ server/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Path = require('path');
+const server = new Hapi.Server();
+
+server.connection({ port: process.env.PORT || 3000 });
+
+server.register(require('inert'));
+
+server.route({
+	method : 'GET', path : '/{param*}',
+	handler : {
+		directory : {
+			path : './../',
+			listing : false,
+			index : true
+		}
+	}
+
+});
+
+server.start(function () {
+	console.log('Server running at:', server.info.uri);
+});
